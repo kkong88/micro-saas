d8d15d9c4a8d32cb52b3a64ecd027f2509cd0f90
Kelly Kong <kkong1992@gmail.com>
2024-02-06 17:52:40 -0500

    added auth routes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () =>  {
   return (
-    <p>Dashboard page (protected)</p>
+    <div>
+        <p>Dashboard page (protected)</p>
+        <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 }
 
